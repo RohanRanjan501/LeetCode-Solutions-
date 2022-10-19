0a0d376281c2fbb61936c52ab765c9c6f8ee0396
RohanRanjan501 <71114382+RohanRanjan501@users.noreply.github.com>
2022-10-19 09:50:19 +0530

    Time: 1 ms (80.66%), Space: 42.5 MB (60.31%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,33 @@
+class Solution {
+   public int search(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            return mid;
+
+
+        } else if (nums[mid] > target) {
+            // the left half is monotonic, yet still does not satisfy
+            if (nums[left] <= nums[mid] && nums[left] > target) { 
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        } else {
+            // the right half is monotonic, yet still does not satisfy
+            if (nums[right] >= nums[mid] && nums[right] < target) { 
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+    }
+    return -1;
+}
+    }
+               
+            
+            
+ 
