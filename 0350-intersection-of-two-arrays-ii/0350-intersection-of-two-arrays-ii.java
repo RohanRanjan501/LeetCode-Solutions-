7edfe49f97703c4a51497a448ca888d1077f92a5
RohanRanjan501 <71114382+RohanRanjan501@users.noreply.github.com>
2022-10-23 10:33:10 +0530

    Time: 2 ms (97.36%), Space: 44 MB (40.22%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,31 +1,51 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
        
-       Arrays.sort(nums1);
-        Arrays.sort(nums2);
+//         Arrays.sort(nums1);
+//         Arrays.sort(nums2);
         
-         ArrayList<Integer> arr = new ArrayList<>();
+//          ArrayList<Integer> arr = new ArrayList<>();
         
-        int i = 0, j = 0;      
-        while(i < nums1.length && j < nums2.length){
-            if(nums1[i]==nums2[j]){
-                arr.add(nums1[i]);
-                i++;
-                j++;
+//         int i = 0, j = 0;      
+//         while(i < nums1.length && j < nums2.length){
+//             if(nums1[i]==nums2[j]){
+//                 arr.add(nums1[i]);
+//                 i++;
+//                 j++;
                 
+//             }
+//             else if(nums1[i]>nums2[j]){
+//                 j++;
+//                                       }
+//             else
+//                 i++;          
+//         }  
+//         int[] result = new int[arr.size()];
+//         for(int a = 0; a < arr.size(); a++){
+//             result[a] = arr.get(a);
+//         }
+//         return result;
+//     }
+        
+        int arr[] = new int[1001];
+        int k = 0;
+        ArrayList<Integer> list = new ArrayList();
+        
+        for(int i = 0; i < nums1.length; i++){
+            arr[nums1[i]]++;
+        }
+        
+        for(int i = 0; i< nums2.length; i++){
+            if(arr[nums2[i]]-- > 0){
+                nums1[k++] = nums2[i];
             }
-            else if(nums1[i]>nums2[j]){
-                j++;
-                                      }
-            else
-                i++;
-                
         }
         
-        int[] result = new int[arr.size()];
-        for(int a = 0; a < arr.size(); a++){
-            result[a] = arr.get(a);
-        }
-        return result;
+        return Arrays.copyOf(nums1,k);
     }
+        
+        
+        
+        
+        
 }
