d0d8238257c629fd8c340b964a8189376fcb8f3a
RohanRanjan501 <71114382+RohanRanjan501@users.noreply.github.com>
2022-11-06 18:27:42 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (23.24%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,47 +1,41 @@
-public class Solution {
-    public List<Integer> spiralOrder(int[][] matrix) {
-        
-        List<Integer> res = new ArrayList<Integer>();
-        
-        if (matrix.length == 0) {
-            return res;
-        }
-        
-        int rowstart = 0;
-        int rowEnd = matrix.length-1;
-        int colstart = 0;
-        int colEnd = matrix[0].length - 1;
-        
-        while (rowstart <= rowEnd && colstart <= colEnd) {
-            // Traverse Right
-            for (int j = colstart; j <= colEnd; j ++) {
-                res.add(matrix[rowstart][j]);
-            }
-            rowstart++;
-            
-            // Traverse Down
-            for (int j = rowstart; j <= rowEnd; j ++) {
-                res.add(matrix[j][colEnd]);
-            }
-            colEnd--;
-            
-            if (rowstart <= rowEnd) {
-                // Traverse Left
-                for (int j = colEnd; j >= colstart; j --) {
-                    res.add(matrix[rowEnd][j]);
-                }
-            }
-            rowEnd--;
-            
-            if (colstart <= colEnd) {
-                // Traver Up
-                for (int j = rowEnd; j >= rowstart; j --) {
-                    res.add(matrix[j][colstart]);
-                }
-            }
-            colstart ++;
-        }
-        
-        return res;
+
+public class Solution{
+public List<Integer> spiralOrder(int[][] matrix) {
+    List<Integer> result = new LinkedList<Integer>();
+    if(matrix == null || matrix.length == 0) return result;    
+    int rowStart = 0;
+    int rowEnd = matrix.length-1;
+    int colStart = 0;
+    int colEnd = matrix[0].length - 1;
+    int direction = 0;
+    
+    while(rowStart <= rowEnd && colStart <= colEnd) {
+        switch(direction) {
+                
+            case 0:   
+                for(int j = colStart; j <= colEnd; j++)           //Right
+                    result.add(matrix[rowStart][j]);
+                rowStart++;
+                break;
+            case 1:                                              //Down
+                for(int j = rowStart; j <=rowEnd; j++) 
+                    result.add(matrix[j][colEnd])    ;
+                colEnd--;
+                break;
+            case 2:
+                for(int j = colEnd; j >= colStart; j --)          //Left
+                    result.add(matrix[rowEnd][j]);            
+                rowEnd--;
+                break;
+            case 3:
+                for(int j = rowEnd; j >= rowStart; j--)           //up
+                    result.add(matrix[j][colStart]);
+                colStart++;  
+                break;
+        }                                   
+        direction = (direction+1)%4;                                                            
     }
+    
+    return result;
+}
 }
