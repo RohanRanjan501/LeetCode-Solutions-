9fc3b6b62740a59b986e89f9067c01b0c1ec9564
RohanRanjan501 <71114382+RohanRanjan501@users.noreply.github.com>
2022-11-07 11:43:02 +0530

    Time: 3 ms (30.20%), Space: 48.6 MB (37.45%) - LeetHub

---
+++ 0867-transpose-matrix/0867-transpose-matrix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        
+        int m = matrix.length;
+        int n = matrix[0].length; //size
+        
+        int result[][] = new int[n][m];
+        
+        for(int i=0; i<m; i++){
+            for(int j=0;j<n; j++){
+                  result[j][i] = matrix[i][j];
+            }
+        }
+        return result;
+    }
+}
