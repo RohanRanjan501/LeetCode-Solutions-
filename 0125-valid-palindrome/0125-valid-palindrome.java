acead20f39b953a446e2e16113b443225499e39f
RohanRanjan501 <71114382+RohanRanjan501@users.noreply.github.com>
2022-11-10 20:20:29 +0530

    Time: 6 ms (77.97%), Space: 44.2 MB (46.89%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -15,3 +15,18 @@ class Solution {
         return true;
     }
 }
+
+// class Solution {
+//     public boolean isPalindrome(String s) {
+        
+//         s = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+//         int start = 0;
+//         int end = s.length()-1;
+//         while(start<end)
+//         {
+//             if(s.charAt(start++)!=s.charAt(end--))
+//                 return false;
+//         }
+//         return true;
+//     }
+// }
