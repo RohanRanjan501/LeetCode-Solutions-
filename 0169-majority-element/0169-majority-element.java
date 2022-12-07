9f27e6061ca3e7c22f80c477040401ab79c9a642
RohanRanjan501 <71114382+RohanRanjan501@users.noreply.github.com>
2022-12-07 11:30:50 +0530

    Time: 30 ms (21.45%), Space: 54.5 MB (79.48%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,21 +3,14 @@ class Solution {
         HashMap<Integer, Integer> m = new HashMap<>();
         
         for(int i=0;i<nums.length;i++){
-            if(m.containsKey(nums[i])){
-                m.put(nums[i],m.get(nums[i])+1);
-            }
-            else{
-                m.put(nums[i],1);
-            }
+            if(m.containsKey(nums[i]))
+                m.put(nums[i],m.get(nums[i])+1);      
+            else m.put(nums[i],1);
         }
-        
         for(int key:m.keySet()){
-            if(m.get(key) > nums.length/2){
-                return key;
-            }
-        }
-        
-        return 0;
-        
+            if(m.get(key) > nums.length/2)
+                return key; 
+        }    
+        return 0;    
     }
 }
