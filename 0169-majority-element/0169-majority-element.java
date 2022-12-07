d9e31e6ebddc7e7bb9f78b07920688109bd68429
RohanRanjan501 <71114382+RohanRanjan501@users.noreply.github.com>
2022-12-07 12:09:32 +0530

    Time: 1 ms (99.93%), Space: 45.5 MB (97.86%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,16 +1,38 @@
-class Solution {
-    public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> m = new HashMap<>();
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         HashMap<Integer, Integer> m = new HashMap<>();
         
-        for(int i=0;i<nums.length;i++){
-            if(m.containsKey(nums[i]))
-                m.put(nums[i],m.get(nums[i])+1);      
-            else m.put(nums[i],1);
+//         for(int i=0;i<nums.length;i++){
+//             if(m.containsKey(nums[i])) 
+//                 m.put(nums[i],m.get(nums[i])+1); //if key already exists increasing the value by 1    
+//             else m.put(nums[i],1);// if the key dosent exitst then adding the new key in the map with value 1
+//         }
+//         for(int key:m.keySet()){ // adding all the key in the keyset and traversing to find if the value is >n/2
+//             if(m.get(key) > nums.length/2)
+//                 return key; 
+//         }    
+//         return 0;    
+//     }
+// }
+
+
+class Solution{
+    public int majorityElement(int [] nums){
+        if(nums.length==1){
+            return nums[0];
         }
-        for(int key:m.keySet()){
-            if(m.get(key) > nums.length/2)
-                return key; 
-        }    
-        return 0;    
+        
+        int ME = 0, count = 1;
+        for(int i = 1; i<nums.length;i++){
+            if(nums[i]==nums[ME]) count++;
+            else
+                count--;
+            
+            if(count == 0){
+                ME = i;
+                count = 1;
+            }
+        }
+        return nums[ME];
     }
 }
